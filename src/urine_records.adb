a754afea2d8fa70f1294766a1b43919c6c43b6dc
Ross Summerfield <ross@cicada.canberra.hyperquantum.com.au>
2021-03-21 17:51:21 +1100

    Update urine_records.adb.  The edition is a functioning, albeit incomplete, main program for urine records.  It still needs some of the initial configuration done.

--- src/urine_records.adb
+++ src/urine_records.adb
@@ -26,34 +26,52 @@
 --  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  --
 --  Licence for  more details.  You should have received a copy  of  --
 --  the GNU General Public Licence distributed with  Urine_Records.  --
---  If not, write to the Free Software Foundation, 59 Temple Place-  --
---  Suite 330, Boston, MA 02111-1307, USA.                           --
+--  If  not,  write to the Free Software  Foundation,  51  Franklin  --
+--  Street, Fifth Floor, Boston, MA 02110-1301, USA.                 --
 --                                                                   --
 -----------------------------------------------------------------------
 
-with Gtk.Main, Gtk.Window;
 with Urine_Record_Version;
+with Main_Menu;
+with Error_Log;
+with dStrings; use dStrings;
+with Generic_Command_Parameters;
+with GNATCOLL.SQL.Sqlite;   -- or Postgres
+with GNATCOLL.SQL.Exec;
 
 procedure Urine_Records is
-   Window : Gtk.Window.Gtk_Window;
 
    default_log_file_name : constant wide_string := 
                            "/var/log/urine_records.log";
    default_db_name       : constant wide_string := 
                            "/var/local/urine_records.db";
+   default_tex_name      : constant wide_string := 
+                           "/usr/bin/pdflatex";
+   default_pdf_name      : constant wide_string := 
+                           "/usr/bin/xpdf";
+   default_R_name        : constant wide_string := 
+                           "/usr/bin/R";
    
    package Parameters is new Generic_Command_Parameters
       (Urine_Record_Version.Version,
-       "p,port,integer,5000,local port number to listen on;" &
-       "x,db,string,"& default_db_name &
-                 ",file name for the switch database XML;"&
+       "x,db,string," & default_db_name &
+                 ",path and file name for the urine records database;" &
+       "t,tex,string," & default_tex_name &
+                 ", Path to LaTex PDF output generator;" & 
+       "p,pdf,string," & default_pdf_name &
+                 ", Path to PDF display tool;" &
+       "r,R,string," & default_R_name &
+                 ", Path to GNU R graph generating tool;" &
        "l,log,string," & default_log_file_name & 
                  ",log file name with optional path;" &
        "d,debug,integer,0,debug level (0=none, 9=max)",
        0, false);
    use Parameters;
 
-
+   DB_Descr : GNATCOLL.SQL.Exec.Database_Description;
+   tex_path : text := Parameter(with_flag => flag_type'('t'));
+   pdf_path : text := Parameter(with_flag => flag_type'('p'));
+   R_path   : text := Parameter(with_flag => flag_type'('r'));
 begin
    Urine_Record_Version.Register(revision => "$Revision: 1.0 $",
       for_module => "Urine_Records");
@@ -63,8 +81,18 @@ begin
       -- abort Urine_Records;
       return;
    end if;
-   Gtk.Main.Init;
-   Gtk.Window.Gtk_New (Window);
-   Gtk.Window.Show (Window);
-   Gtk.Main.Main;
+   Error_Log.Set_Log_File_Name(
+      Value(Parameter(with_name=>Value("log"))));
+   Error_Log.Set_Debug_Level
+         (to => Parameter(with_flag => flag_type'('d')) );
+   Error_Log.Debug_Data(at_level => 1, 
+                        with_details => "Urine_Records: Start processing");
+   -- Set up the database
+   DB_Descr := GNATCOLL.SQL.Sqlite.Setup
+                            (Value(Parameter(with_flag => flag_type'('x'))));
+   -- Bring up the main menu
+   Main_Menu.Initialise_Main_Menu(Parameters.The_Usage, DB_Descr, 
+                                  tex_path, pdf_path, R_path);
+   -- Free up the database description when done
+   GNATCOLL.SQL.Exec.Free (DB_Descr);
 end Urine_Records;
